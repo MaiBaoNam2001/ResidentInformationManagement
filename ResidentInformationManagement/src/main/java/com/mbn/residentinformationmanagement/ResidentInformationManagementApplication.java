497b6e24a5135c4f24475e26922f9cf8c36063af
MaiBaoNam2001 <maibaonam260219@gmail.com>
2022-10-06 10:54:55 +0700

    init-resident-information-management

---
+++ ResidentInformationManagement/src/main/java/com/mbn/residentinformationmanagement/ResidentInformationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mbn.residentinformationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidentInformationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResidentInformationManagementApplication.class, args);
+    }
+
+}
---
+++ ResidentInformationManagement/src/test/java/com/mbn/residentinformationmanagement/ResidentInformationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbn.residentinformationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidentInformationManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

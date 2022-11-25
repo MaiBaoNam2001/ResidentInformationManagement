9722c77364cdee56019c898f21ecefc8a3ec7126
MaiBaoNam2001 <maibaonam260219@gmail.com>
2022-11-25 09:54:50 +0700

    do: add IdentityCardValidation

---
+++ ResidentInformationManagement/src/main/java/com/citynow/residentinformationmanagement/service/IdentityCardValidation.java
@@ -0,0 +1,24 @@
+package com.citynow.residentinformationmanagement.service;
+
+import com.citynow.residentinformationmanagement.service.template.IService;
+import lombok.Data;
+
+public interface IdentityCardValidation extends
+    IService<IdentityCardValidation.Input, IdentityCardValidation.Output> {
+
+  @Data
+  class Input {
+
+    private String projectId;
+    private String buildingId;
+    private String parkingAreaId;
+    private String identityCard;
+  }
+
+  @Data
+  class Output {
+
+    private int statusCode;
+    private Object result;
+  }
+}
